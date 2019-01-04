7cdcc69d7e71e1dab5f03892fb814a7b413fbd75
2dingqing <442367569@qq.com>
2019-01-04 23:34:08 +0800

    commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# chatroom
+基于Swoole实现简易聊天室
+
+
+## 环境要求
+- PHP >= 7.0
+- Swoole
+- composer
+
+## 安装
+- composer install
+
+## 启动
+- 启动 websocket
+```
+php webim_server.php
+```
+- 启动 PHP 内置服务器
+```
+cd ./public
+php -S localhost:8000
+```
---
+++ config/chatroom.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'ip' => '0.0.0.0',
+    'port' => '9501',
+    'avatar' => [
+        './images/avatar/1.jpg',
+        './images/avatar/2.jpg',
+        './images/avatar/3.jpg',
+        './images/avatar/4.jpg',
+        './images/avatar/5.jpg',
+        './images/avatar/6.jpg'
+    ],
+    'name' => [
+        '马云',
+        '马化腾',
+        '雷军',
+        '张一鸣',
+        '李彦宏',
+        '程维',
+        '王兴',
+    ]
+];
